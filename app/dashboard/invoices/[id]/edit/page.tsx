028008aba14cded28da2b35fc29e703e2312f273
jmriesen <jacobriesen@gmail.com>
2024-10-08 17:50:30 -0500

    Copying in code from tutorial.

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,3 +1,20 @@
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+
 export default async function Page() {
-	return <p>temp </p>
+	return (
+		<main>
+			<Breadcrumbs
+				breadcrumbs={[
+					{ label: 'Invoices', href: '/dashboard/invoices' },
+					{
+						label: 'Edit Invoice',
+						href: `/dashboard/invoices/${id}/edit`,
+						active: true,
+					},
+				]}
+			/>
+			<Form invoice={invoice} customers={customers} />
+		</main>
+	);
 }
